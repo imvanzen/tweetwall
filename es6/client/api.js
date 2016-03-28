eaf3caf6d7999df617e458396199a42d63e62d07
Jakub Reczko <contact@jakubreczko.com>
2016-03-28 15:49:59 +0200

    changed default hosts

--- es6/client/api.js
+++ es6/client/api.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const timeline = () => {
-  return new WebSocket(`ws://localhost:8080/timeline.io`, `protocolOne`)
+  return new WebSocket(`ws://localhost:5000/timeline.io`, `protocolOne`)
 }
 
 export default {
