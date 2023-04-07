6cdc08b562f6bb03993259c257c8c7f2cf16b397
Jakub Reczko <jakub.r@masterborn.com>
2023-04-07 10:35:58 +0200

    refactor: replace with nextjs

--- es6/client/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-import React from 'react'
-import ReactDom from 'react-dom'
-import TweetWall from './components/tweetWall'
-
-import './styles/main.scss'
-
-ReactDom.render(<TweetWall />, document.getElementById('TweetWall'))
